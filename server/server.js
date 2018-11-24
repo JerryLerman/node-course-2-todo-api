58eb8b907cf418e7c7a14b016b7e917bf64deb73
Jerry Lerman <lerman@runbox.com>
2018-11-24 16:30:56 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,14 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 // Go to httpstatuses.com to see all status code
 
 app.listen(3000, () => {
