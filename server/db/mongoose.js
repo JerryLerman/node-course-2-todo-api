0c91d88733dcb2b55e6fc82986e73f05e8c5cd2a
Jerry Lerman <lerman@runbox.com>
2018-12-01 13:18:31 -0500

    Setup separate test and development envs:

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,11 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
 // Use local mongo db if not running under Heroku
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
+
+
+// Just about every hosting company sets this variable
+// process.env.NODE_ENV === 'production'; //Usually the default value
+// can be 'development' and 'test'
